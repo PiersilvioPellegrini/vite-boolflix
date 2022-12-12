b2fd88c37bfa94f6fb7b9694fba4b3950a6a0b2e
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2022-12-12 17:12:00 +0100

    Created Scaffold Project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+// import Componente from './components/Componente';
+import TheHeader from './components/TheHeader.vue';
+import TheSelect from './components/TheSelect.vue';
+import TheMain from './components/TheMain.vue';
+
+// importo file esterno con i dati
+import { FunzionePerApi, store } from "./store";
+
+export default {
+  components: { TheHeader,TheSelect,TheMain },
+  data () {
+    return {
+      
+      store
+    };
+  },
+  methods: {
+    // Search (filtersEmitted) {
+    //   this.store.Filters = filtersEmitted;
+    //   fetchCharacters();
+
+    //   console.log(filtersEmitted);
+    // }
+  },
+  }
+
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+
+@use "./styles/general.scss";
+
+</style>
